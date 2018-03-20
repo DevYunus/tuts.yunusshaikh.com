de54c2348ca41d6b24c9f520dabe677f1bee3e78
yunus shaikh <yunus.work829@gmail.com>
2018-03-20 17:16:50 +0530

    milestone-1

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,3 +21,25 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Thread::class,function (Faker $faker){
+    return [
+        'title'=> $faker->sentence,
+        'body'=> $faker->paragraph,
+        'user_id'=>function(){
+            return factory('App\User')->create()->id;
+        }
+    ];
+});
+
+$factory->define(App\Reply::class,function (Faker $faker){
+    return [
+        'body'=> $faker->paragraph,
+        'user_id'=>function(){
+            return factory('App\User')->create()->id;
+        },
+        'thread_id'=>function(){
+            return factory('App\Thread')->create()->id;
+        }
+    ];
+});
---
+++ database/migrations/2018_03_20_110408_create_replies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('thread_id');
+            $table->string('body');
+            $table->timestamps();
+
+            /*$table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('thread_id')->references('id')->on('threads');*/
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
